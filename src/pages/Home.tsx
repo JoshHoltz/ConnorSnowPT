c7d05c679729820fe62d01139c0499e4eca03bbb
Josh <joshua.holtz@outlook.com>
2025-05-25 14:20:32 +0100

    reverted properly

---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+// import React from 'react'
+import { Hero } from '../components/Hero'
+import { MembershipTiers } from '../components/MembershipTiers'
+import { CTA } from '../components/CTA'
+export const Home = () => {
+  return (
+    <>
+      <Hero />
+      <MembershipTiers />
+      <CTA />
+    </>
+  )
+}
