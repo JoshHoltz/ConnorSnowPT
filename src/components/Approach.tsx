318a6ba252bcdbf0b951df7f74a68b7708ed6cca
Josh <joshua.holtz@outlook.com>
2025-05-26 12:28:16 +0100

    Approach Icons Added

--- src/components/Approach.tsx
+++ src/components/Approach.tsx
@@ -1,5 +1,7 @@
 import React from "react";
-import { ArrowRightIcon } from "lucide-react";
+import { Heart } from 'lucide-react';
+import { User } from 'lucide-react';
+import { Trophy } from 'lucide-react';
 
 export const Approach = () => {
   return (
@@ -12,20 +14,20 @@ export const Approach = () => {
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
           
           <div className="border-t-4 bg-white p-8 flex flex-col h-full relative shadow-md hover:bg-gray-300 transition duration-300 ease-in-out">
-            <h3 className="text-xl font-semibold mb-2">"Amazing results!"</h3>
-            <p className="mb-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia optio nobis dolor ex fugit perspiciatis iusto accusantium, quod sed eveniet! Autem, eius eum corrupti aspernatur nam quod rerum natus. Qui.</p>
+            <div className="flex items-center gap-2"><Heart className="text-red-500" /> <span className="text-xl font-bold">Lorem</span></div>
+            <p className="mb-4 mt-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia optio nobis dolor ex fugit perspiciatis iusto accusantium, quod sed eveniet! Autem, eius eum corrupti aspernatur nam quod rerum natus. Qui.</p>
           </div>
 
           <div className="border-t-4 bg-white p-8 flex flex-col h-full relative shadow-md hover:bg-gray-300 transition duration-300 ease-in-out">
-            <h3 className="text-xl font-semibold mb-2">"Supportive & effective"</h3>
-            <p className="mb-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis harum error corrupti sint porro illo aliquam natus beatae at tenetur, odit ut, reiciendis laboriosam hic sunt ex asperiores dignissimos consequatur!</p>
+            <div className="flex items-center gap-2"><User className="text-red-500" /> <span className="text-xl font-bold">Lorem</span></div>
+            <p className="mb-4 mt-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia optio nobis dolor ex fugit perspiciatis iusto accusantium, quod sed eveniet! Autem, eius eum corrupti aspernatur nam quod rerum natus. Qui.</p>
           </div>
 
           <div className="border-t-4 bg-white p-8 flex flex-col h-full relative shadow-md hover:bg-gray-300 transition duration-300 ease-in-out">
-            <h3 className="text-xl font-semibold mb-2">"Professional & motivating"</h3>
-            <p className="mb-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt architecto voluptate, nulla nisi provident autem qui soluta a unde doloribus corporis pariatur, repellendus nihil laborum minima magni animi adipisci praesentium!
-            </p>
+            <div className="flex items-center gap-2"><Trophy className="text-red-500" /> <span className="text-xl font-bold">Lorem</span></div>
+            <p className="mb-4 mt-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia optio nobis dolor ex fugit perspiciatis iusto accusantium, quod sed eveniet! Autem, eius eum corrupti aspernatur nam quod rerum natus. Qui.</p>
           </div>
+
         </div>
       </div>
     </section>
