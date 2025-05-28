589e93606651ac77901c20132461aa1c0bb85649
Joshua Holtz <Joshua.Holtz@outlook.com>
2025-05-28 11:41:24 +0100

    CTA.tsx mobile responsive

--- src/components/CTA.tsx
+++ src/components/CTA.tsx
@@ -5,18 +5,17 @@ export const CTA = () => {
     <section className="bg-black text-white py-20 m-6">
       <div className="container mx-auto px-4 flex flex-col md:flex-row items-center justify-center">
         <div className="w-full space-y-8">
-          <h1 className="text-2xl md:text-3xl font-bold mb-2">
+          <h1 className="text-2xl text-center md:text-left md:text-3xl font-bold mb-2">
             Wanting to get started? Join Me Now!
           </h1>
-          <p className="flex flex-col md:flex-row">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo
-            accusantium sunt.
+          <p className="text-center md:text-left">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo accusantium sunt.
           </p>
         </div>
 
         <div className="w-full mt-10 md:mt-0">
-          <div className="relative flex justify-end">
-<a href="/packages"><button className="bg-blue-600 hover:bg-blue-700 transition px-6 py-3 font-bold flex items-start gap-2">
+          <div className="relative flex justify-center md:justify-end">
+            <a href="/packages"><button className="bg-blue-600 hover:bg-blue-700 transition px-6 py-3 font-bold flex items-start gap-2">
               Start Your Journey
               <ArrowRightIcon className="w-5 h-5" />
             </button></a>
