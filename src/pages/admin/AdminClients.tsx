71c4e11d19110a51edf5ac894ecd8de13c10af63
Josh <joshua.holtz@outlook.com>
2025-06-03 22:27:00 +0100

    px-8 changed from px-6 to allign text

--- src/pages/admin/AdminClients.tsx
+++ src/pages/admin/AdminClients.tsx
@@ -5,8 +5,8 @@ import { MembersTable } from '../../components/admin/MembersTable'
 export const AdminClients = () => {
   return (
         <div className="mb-4 text-black">
-      <h1 className="hidden md:flex text-2xl font-bold text-black px-6">Clients</h1>
-      <p className="hidden md:flex text-black px-6">
+      <h1 className="hidden md:flex text-2xl font-bold text-black px-8">Clients</h1>
+      <p className="hidden md:flex text-black px-8">
         View and Manage Your Clients
       </p>
     <TopLevelInfo />
--- src/pages/admin/AdminHome.tsx
+++ src/pages/admin/AdminHome.tsx
@@ -5,8 +5,8 @@ import { QuickLinks } from '../../components/admin/QuickLinks'
 export const AdminHome = () => {
   return (
     <div className="mb-4 text-black">
-      <h1 className="hidden md:flex text-2xl font-bold text-black px-6">Dashboard</h1>
-      <p className="hidden md:flex text-black px-6">
+      <h1 className="hidden md:flex text-2xl font-bold text-black px-8">Dashboard</h1>
+      <p className="hidden md:flex text-black px-8">
         Welcome Back! Take a look at your client overview:
       </p>
 
