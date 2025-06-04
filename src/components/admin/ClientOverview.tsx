045e6a33afa7175d4c5c2c79915211b523d339bc
Josh <joshua.holtz@outlook.com>
2025-06-04 15:33:00 +0100

    not sure why not working but oh well

--- src/components/admin/ClientOverview.tsx
+++ src/components/admin/ClientOverview.tsx
@@ -1,22 +1,29 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 
-export const ClientOverview = ({ id }) => {
+export const ClientOverview = () => {
   const [client, setClient] = useState(null);
+  const location = useLocation();
+
+  const queryParams = new URLSearchParams(location.search);
+  const id = queryParams.get("id");
 
   useEffect(() => {
     if (!id) return;
     fetch(`https://connorsnowpt.onrender.com/api/client-by-id/${id}`)
-      .then(res => res.ok ? res.json() : Promise.reject("Failed to fetch client"))
+      .then((res) =>
+        res.ok ? res.json() : Promise.reject("Failed to fetch client")
+      )
       .then(setClient)
-      .catch(err => console.error(err));
+      .catch((err) => console.error(err));
   }, [id]);
 
   return (
-    <section className="text-white p-4 mt-10 md:mt-0">
-      <div className="mb-4 px-4 text-black">
-        <h2>
-          {client ? `${client.client_firstname} ${client.client_lastname}` : 'Client not found'}
-        </h2>
+    <section className=" text-white p-4 mt-10 md:mt-0">
+      <div className="mb-4 px-4 text-black bg-gray-800">
+        <h1 className="text-white p-4 font-bold text-2xl">
+          {client?.client_firstname + " " + client.client_lastname ?? "Loading client..."}
+        </h1>
       </div>
     </section>
   );
