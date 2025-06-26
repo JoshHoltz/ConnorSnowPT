91a673418d822131343392d9985c52fd096852d8
Josh <joshua.holtz@outlook.com>
2025-06-26 16:08:52 +0100

    booking page added

--- src/components/admin/Calendar.tsx
+++ src/components/admin/Calendar.tsx
@@ -13,3 +13,23 @@ export const Calendar = () => {
     </div>
   );
 };
+
+// <!-- Google Calendar Appointment Scheduling begin -->
+// <link href="https://calendar.google.com/calendar/scheduling-button-script.css" rel="stylesheet">
+// <script src="https://calendar.google.com/calendar/scheduling-button-script.js" async></script>
+// <script>
+// (function() {
+//   var target = document.currentScript;
+//   window.addEventListener('load', function() {
+//     calendar.schedulingButton.load({
+//       url: 'https://calendar.google.com/calendar/appointments/schedules/AcZssZ2awze57wY62kAINPUmx7QtFX8L0vU9oWh3uYgF40N_-QZpK5tGa4o7js8mXEX4mBp2LZPZX6aE?gv=true',
+//       color: '#039BE5',
+//       label: 'Book an appointment',
+//       target,
+//     });
+//   });
+// })();
+// </script>
+// <!-- end Google Calendar Appointment Scheduling --></link>
+
+// https://calendar.app.google/heqPAmJgpVE7JQhF9
