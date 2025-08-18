be553405229a69da43a1329501c3864725b223d7
Josh <joshua.holtz@outlook.com>
2025-08-18 11:48:58 +0100

    posthog post setup intergrated at index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,35 @@
 // src/index.tsx
-import './index.css'
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { App } from './App'
-import { AdminApp } from './AdminApp'
-import { ClientApp } from './ClientApp'
+import "./index.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { App } from "./App";
+import { AdminApp } from "./AdminApp";
+import { ClientApp } from "./ClientApp";
+import { PostHogProvider } from "posthog-js/react";
 
-const pathname = window.location.pathname
+const pathname = window.location.pathname;
 
-let RenderApp = App
+let RenderApp = App;
 
-if (pathname.startsWith('/admin')) {
-  RenderApp = AdminApp
-} else if (pathname.startsWith('/client')) {
-  RenderApp = ClientApp
+if (pathname.startsWith("/admin")) {
+  RenderApp = AdminApp;
+} else if (pathname.startsWith("/client")) {
+  RenderApp = ClientApp;
 }
 
-const root = ReactDOM.createRoot(document.getElementById('root')!)
+const options = {
+  api_host: 'https://eu.i.posthog.com',
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root")!);
 
 root.render(
   <React.StrictMode>
-    <RenderApp />
+    <PostHogProvider
+      apiKey={'phc_NuDmT8JTMJYJcd3N1u3zgoVeiL05Wklt28nW8rVzhwk'}
+      options={options}
+    >
+      <RenderApp />
+    </PostHogProvider>
   </React.StrictMode>
-)
+);
